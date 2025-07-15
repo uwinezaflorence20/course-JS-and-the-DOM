12ca8706ffec36b38ccbc5d8f9ddc3234f6745c5
Uwineza Florence <uwinezaflorence20@gmail.com>
2025-07-15 08:33:00 +0200

    done

--- app.js
+++ app.js
@@ -6,4 +6,39 @@ document.addEventListener("click", function() {
    const mainHeading = document.querySelector('h1');
    mainHeading.style.backgroundColor = 'red';
 });
-s
+
+document.addEventListener("dblclick", function(){
+    console.log("double clicked");
+});
+document.addEventListener("mouseove", function(){
+    console.log("mouse ove");
+});
+document.addEventListener("mouseenter", function(){
+    console.log("mouse enterd");
+});
+
+document.addEventListener("mousemove", function(){
+    console.log("mouse moved");
+});
+document.addEventListener("mousedown", function(){
+    console.log("mouse moved down");
+});
+
+document.addEventListener("mouseup", function(){
+    console.log("mouse moved up");
+});
+
+document.addEventListener("dblclick", function(){
+    console.log("double clicked");
+});
+
+document.addEventListener("keydown", function(){
+    console.log("key down");
+});
+
+document.addEventListener("keypress", function(){
+    console.log("key pressed");
+});
+document.addEventListener("keyup", function(){
+    console.log("key up");
+});
